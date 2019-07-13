49f871723ad3567ffe71ad1d15ad681270e69073
ZhangWeisai <weisaizhang@gmail.com>
2019-07-13 21:22:28 +0800

    Change sendrawTransacion for bet

--- contract/deploy/deploy.js
+++ contract/deploy/deploy.js
@@ -16,6 +16,7 @@ var from1 = '0x122369f04f32269598789998de33e3d56e2c507a';
 var from1Keystore = 'replace';
 var pass1 = '';
 var from2 = '0x3dcf0b3787c31b2bdf62d5bc9128a79c2bb18829';
+var from2Keystore = 'replace';
 var pass2 = '';
 var toAddr = '0x3ea7a559e44e8cabc362ca28b6211611467c76f3';
 
@@ -33,34 +34,12 @@ var abi = JSON.parse(wasmabi.toString('utf-8'));
 
 function deployWasmContractWithPrivateKey() {
   var contract = vnt.core.contract(abi).codeFile(codeFile);
-  var deployContract = contract.packContructorData();
 
+  var deployContract = contract.packContructorData();
+  var value = vnt.toHex(vnt.toWei(100, 'vnt'))
   var account = vntkit.account.decrypt(from1Keystore, pass1, false);
-  var nonce = vnt.core.getTransactionCount(account.address);
-  var options = {
-    nonce: nonce,
-    gasPrice: vnt.toHex(vnt.toWei(18, 'Gwei')),
-    gasLimit: vnt.toHex(4000000),
-    data: deployContract,
-    value: vnt.toHex(vnt.toWei(100, 'vnt')),
-    chainId: chainid
-  };
-  var tx = new TX(options);
-  tx.sign(new Buffer(
-    account.privateKey.substring(
-          2,
-          ),
-      'hex'));
-  var serializedTx = tx.serialize();
-  vnt.core.sendRawTransaction(
-    '0x' + serializedTx.toString('hex'), function(err, txHash) {
-      if (err) {
-        console.log('err happened: ', err)
-        console.log('transaction hash: ', txHash);
-      } else {
-        console.log('transaction hash: ', txHash);
-      }
-    });
+  
+  sendRawTransaction(account, "0x0", deployContract, value)
 }
 
 function getTransactionReceipt(tx, cb) {
@@ -88,18 +67,46 @@ function Deposit() {
 
 
 function Bet() {
-  var contract = vnt.core.contract(abi).at(contractAddress);
-  var r = contract.Bet.sendTransaction(vnt.toWei(10, 'vnt'), 1, {
-    from: from2,
-    gas: 4000000,
-  });
-  console.log('bet', r);
+  var contract = vnt.core.contract(abi);
+  var data = contract.packFunctionData("Bet", [vnt.toWei(10), 1]);
+  var account = vntkit.account.decrypt(from2Keystore, pass2, false);
+
+  sendRawTransaction(account, contractAddress, data, vnt.toHex(0))
 }
 
 function TestRandom() {
   var contract = vnt.core.contract(abi).at(contractAddress);
   var r = contract.testRandom.call({from: from1});
   console.log('random', r.toString());
+}
+
+function sendRawTransaction(account,to,data,value){
+  var nonce = vnt.core.getTransactionCount(account.address);
+  var options = {
+    nonce: nonce,
+    to: to,
+    gasPrice: vnt.toHex(vnt.toWei(18, 'Gwei')),
+    gasLimit: vnt.toHex(4000000),
+    data: data,
+    value: value,
+    chainId: chainid
+  };
+  var tx = new TX(options);
+  tx.sign(new Buffer(
+    account.privateKey.substring(
+          2,
+          ),
+      'hex'));
+  var serializedTx = tx.serialize();
+  vnt.core.sendRawTransaction(
+    '0x' + serializedTx.toString('hex'), function(err, txHash) {
+      if (err) {
+        console.log('err happened: ', err)
+        console.log('transaction hash: ', txHash);
+      } else {
+        console.log('transaction hash: ', txHash);
+      }
+    });
 }
 
 
